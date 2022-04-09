6cd384451a6cdf35d1e2d8de4ac3867bbf30a89a
Sahircan Sürmeli <ssahircan@gmail.com>
2022-04-09 10:45:45 +0200

    Toggle link command

--- src/commands/default-commands/linkCommand.tsx
+++ src/commands/default-commands/linkCommand.tsx
@@ -12,13 +12,42 @@ export const linkCommand: Command = {
       selection: initialState.selection
     });
     const state1 = textApi.setSelectionRange(newSelectionRange);
-    // Replaces the current selection with the bold mark up
-    const state2 = textApi.replaceSelection(`[${state1.selectedText}](url)`);
-    // Adjust the selection to not contain the **
-    textApi.setSelectionRange({
-      start: state2.selection.end - 6 - state1.selectedText.length,
-      end: state2.selection.end - 6
-    });
+
+    const s1 = state1.selection;
+    const text = state1.text;
+    const selectedText = state1.selectedText;
+
+    let state2;
+
+    if (selectedText.substring(0, 1) === "[" && selectedText.substring(selectedText.length - 6, selectedText.length) === "](url)") {
+      state2 = textApi.replaceSelection(selectedText.substring(1, selectedText.length - 6));
+      textApi.setSelectionRange({
+        start: s1.start,
+        end: s1.end - 6
+      });
+    }
+
+    else if (s1.start >= 1 && text.substring(s1.start - 1, s1.start) === "[" && text.substring(s1.end, s1.end + 6) === "](url)") {
+      textApi.setSelectionRange({
+        start: s1.start - 1,
+        end: s1.end + 6
+      });
+      state2 = textApi.replaceSelection(state1.selectedText);
+      textApi.setSelectionRange({
+        start: state2.selection.start - state1.selectedText.length,
+        end: state2.selection.end
+      });
+    }
+
+    else {
+      // Replaces the current selection with the bold mark up
+      state2 = textApi.replaceSelection(`[${state1.selectedText}](url)`);
+      // Adjust the selection to not contain the **
+      textApi.setSelectionRange({
+        start: state2.selection.end - 6 - state1.selectedText.length,
+        end: state2.selection.end - 6
+      });
+    }
   },
   handleKeyCommand: e => (e.ctrlKey || e.metaKey) && e.key == "k"
 };
