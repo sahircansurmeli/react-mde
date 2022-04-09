87de1b90248322e37dedd94dab7fc854038993c3
Sahircan Sürmeli <ssahircan@gmail.com>
2022-04-09 10:50:03 +0200

    Toggle image command

--- src/commands/default-commands/imageCommand.tsx
+++ src/commands/default-commands/imageCommand.tsx
@@ -12,14 +12,42 @@ export const imageCommand: Command = {
         selection: initialState.selection
       })
     );
-    // Replaces the current selection with the image
-    const imageTemplate =
-      state1.selectedText || "https://example.com/your-image.png";
-    textApi.replaceSelection(`![](${imageTemplate})`);
-    // Adjust the selection to not contain the **
-    textApi.setSelectionRange({
-      start: state1.selection.start + 4,
-      end: state1.selection.start + 4 + imageTemplate.length
-    });
+
+    const s1 = state1.selection;
+    const text = state1.text;
+    const selectedText = state1.selectedText;
+
+    let state2;
+
+    if (selectedText.substring(0, 4) === "![](" && selectedText.substring(selectedText.length - 1, selectedText.length) === ")") {
+      state2 = textApi.replaceSelection(selectedText.substring(4, selectedText.length - 1));
+      textApi.setSelectionRange({
+        start: s1.start,
+        end: s1.end - 1
+      });
+    }
+
+    else if (s1.start >= 4 && text.substring(s1.start - 4, s1.start) === "![](" && text.substring(s1.end, s1.end + 1) === ")") {
+      textApi.setSelectionRange({
+        start: s1.start - 4,
+        end: s1.end + 1
+      });
+      state2 = textApi.replaceSelection(state1.selectedText);
+      textApi.setSelectionRange({
+        start: state2.selection.start - state1.selectedText.length,
+        end: state2.selection.end
+      });
+    }
+
+    else {
+      // Replaces the current selection with the image
+      const imageTemplate = state1.selectedText;
+      textApi.replaceSelection(`![](${imageTemplate})`);
+      // Adjust the selection to not contain the **
+      textApi.setSelectionRange({
+        start: state1.selection.start + 4,
+        end: state1.selection.start + 4 + imageTemplate.length
+      });
+    }
   }
 };
